98225d58b3c35d0e175006f6eb2bf050acf73990
Awais Asghar <awaisasghar@Awaiss-MacBook-Pro.local>
2017-06-19 13:42:53 +0500

    initial commit

---
+++ Assets/Scripts/BasicMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour {
+
+	public float spinSpeed = 180.0f;
+	public float motionMagnitude = 0.3f;
+	void Update () {
+		gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+
+		gameObject.transform.Translate (Vector3.down * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+	}
+}
